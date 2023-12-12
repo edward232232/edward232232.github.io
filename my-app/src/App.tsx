70da67fade68f56be39b848ae840eb4621a6100c
Eddie <kong.eddie@gmail.com>
2023-12-11 22:07:29 -0800

    .

---
+++ my-app/src/App.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0">
+        <div>
+          <Navbar />
+          {/* <Hero /> */}
+        </div>
+
+        <div className="bg-center bg-no-repeat bg-cover bg-about">
+          {/* <About /> */}
+        </div>
+
+        <div className="pb-10 bg-center bg-no-repeat bg-cover bg-tech">
+          {/* <Tech /> */}
+        </div>
+
+        {/* <Projects /> */}
+
+        <div
+          className="bg-experience bg-cover bg-center bg-no-repeat 
+            rounded-tl-[150px] rounded-br-[150px]">
+          <div
+            className="bg-experienceLight bg-cover bg-center 
+            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
+            {/* <Experience /> */}
+          </div>
+        </div>
+        <div className="relative z-0">
+          {/* <Contact /> */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
